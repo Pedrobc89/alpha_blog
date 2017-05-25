c6e1d1bbc470b6e14a0ec3ad1e5547c87f496532
pedrobc89@gmail.com <pedrobc89@gmail.com>
2017-05-25 20:44:29 +0000

    added bootstrap-sass to gemfile

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: {minimun: 3, maximun: 50}
-  validates :description, presence: true , length: {minimun: 10, maximun: 300}
+  validates :title, presence: true, length: {minimun: 3, maximum: 50}
+  validates :description, presence: true , length: {minimun: 10, maximum: 300}
 end
 
