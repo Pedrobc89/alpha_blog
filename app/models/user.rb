8b0c8254ff4fa6f76e02d868100d8070d9aafdb9
pedrobc89@gmail.com <pedrobc89@gmail.com>
2017-06-09 16:36:43 +0000

    add user data to article

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   validates :username, presence: true,
             uniqueness: {case_sensitive: false},
-            length: {minimun: 3, maximun: 25}
+            length: { in: 3..25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
             uniqueness: {case_sensitive: false},
