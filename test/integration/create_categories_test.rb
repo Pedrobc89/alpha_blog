6ffba1a2579c14ef161293151d258d22d3ec7a64
pedrobc89@gmail.com <pedrobc89@gmail.com>
2017-06-12 18:49:57 +0000

    Added invalid test case for create_category_test.rb

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -11,6 +11,20 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     
     assert_template 'categories/index'
     assert_match "test_category", response.body
+    assert_select '#error_explanation', false,  "This page should contain no errors"
+  end
+  
+  
+  test "invalid category submission results in failure" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, category: {name: ""}
+    end
+    
+    assert_template 'categories/new'
+    assert_select '#error_explanation'
+    
   end
   
 end
